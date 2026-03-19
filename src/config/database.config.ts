a5b2c97e6f3428cef1f9a793e0d622661564bb63
Kimheng-SOK <sokkimheng008@gmail.com>
2026-03-19 17:43:17 +0700

    update env config to main.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,19 +1,14 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as dotenv from 'dotenv';
 import { Receipt } from 'src/database/entities/receipts.entity';
-
-dotenv.config();
-
-// console.log(process.env);
 
 export const databaseConfig: TypeOrmModule = {
   type: 'postgres',
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  port: process.env.POSTGRES_PORT
-    ? parseInt(process.env.POSTGRES_PORT, 10)
-    : 5432,
+  port: process.env.POSTGRES_PORT,
+  // ? parseInt(process.env.POSTGRES_PORT, 10)
+  // : 5432,
   host: process.env.POSTGRES_HOST,
   entities: [Receipt],
   synchronize: true, // Set to false in production
